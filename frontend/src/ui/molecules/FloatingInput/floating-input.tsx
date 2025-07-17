6a152682f11d78a47770a26d16c4f26d5f769277
enriqueSFranco <enrique.sfranco04@gmail.com>
2025-07-17 14:09:01 -0600

    feat(ui): componente input

--- frontend/src/ui/molecules/FloatingInput/floating-input.tsx
+++ frontend/src/ui/molecules/FloatingInput/floating-input.tsx
@@ -3,21 +3,25 @@ import styles from "./floating-input.module.css";
 
 interface FloatingInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string
+  description?: string
 }
 
-export function FloatingInput({label, ...props}: FloatingInputProps) {
+export function FloatingInput({label, description, ...props}: FloatingInputProps) {
   const inputHintId = useId()
 
   return (
-    <div className={styles.inputGroup}>
-      <input
-        type="text"
-        id={inputHintId}
-        placeholder=" "
-        className={styles.inputGroup__input}
-        {...props}
-      />
-      <label className={styles.inputGroup__label}>{label}</label>
+    <div>
+      <div className={styles.inputGroup}>
+        <input
+          type="text"
+          id={inputHintId}
+          placeholder=" "
+          className={styles.inputGroup__input}
+          {...props}
+        />
+        <label className={styles.inputGroup__label}>{label}</label>
+      </div>
+      {description && <p className={styles.inputGroup__description}>{description}</p>}
     </div>
   );
 }
