35321febce6a0528744087b023a2184478ef319b
enriqueSFranco <enrique.sfranco04@gmail.com>
2025-08-05 19:18:56 -0600

    feat(ui): componente navbar

--- frontend/src/ui/navbar.tsx
+++ frontend/src/ui/navbar.tsx
@@ -1,15 +1,8 @@
 import clsx from "clsx"
 import styles from "./navbar.module.css"
-import Link from "next/link"
-
-interface MenuItems {
-  text: string
-  icon?: React.ReactNode
-}
 
 interface NavbarProps {
   children: React.ReactNode
-
 }
 
 export function Navbar({children}: NavbarProps) {
