768308e521d0d88eaad9da36686a0407818551c4
enriqueSFranco <enrique.sfranco04@gmail.com>
2025-08-05 19:14:17 -0600

    feat(types): tipos para las props que recibe una columna en una tabla

---
+++ frontend/src/ui/organisms/table/types.ts
@@ -0,0 +1,9 @@
+export interface RowItemWhitId {
+  id: string;
+}
+
+export interface ColumnDefinition<T extends RowItemWhitId> {
+  key: keyof T | string;
+  header: string;
+  renderCell?: (item: T) => React.ReactNode;
+}
